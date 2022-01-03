2d40b8163103b5c7483be2cdc1ea33c5b92fcdb3
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2022-01-03 10:43:51 +0100

    Update config.js

--- source/lib/config.js
+++ source/lib/config.js
@@ -2,9 +2,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-export const RABBITMQ_URL = process.env.RABBITMQ_URL;
-export const REDIS_URL = process.env.REDIS_URL;
-
-export const GQL_ENDPOINT = process.env.GQL_ENDPOINT;
-export const GQL_AUTH_TOEKN = process.env.GQL_AUTH_TOEKN;
+export const ENV = process.env.ENV || "DEV";
